97399af4083106d1623e0d13dbfabbb25a79647d
Todd <todd.griffin61@outlook.com>
2024-03-14 11:01:56 +0000

    initial commit, created game plan

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+/*
+ * 
+ * MVP:
+ * 1. The bird will be controlled with a key and move up and down on the screen
+ * 2. Obstacles of varying heights will travel from right to left
+ * 3. The game ends when the bird touches an obstacle
+ *
+ * === # Psuedocode ===
+ *
+ * Typescript will be used for:
+ * - Creating objects
+ * - Holding state
+ * - Taking keyboard inputs from the user
+ * - Detecting events, i.e. collisions and scoring
+ * - Moving objects, i.e. the bird and the obstacles
+ *
+ *
+ * Code structure:
+ * 1. Setup functions
+ *   - Game state
+ *   - Object creation
+ * 2. Game loop
+ *   - Each loop will have a set time interval
+ *   - Game logic happens here
+ *
+ * File structure:
+ * main.ts - contains main game loop
+ * setup.ts - initialises game objects
+ * utils.ts - utility functions
+ *
+ */
+
+
+import './style.css'
+
+
+
