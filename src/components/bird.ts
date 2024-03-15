727ddcd28e1c54714c4b5c33cea6e11158d24912
Todd <todd.griffin61@outlook.com>
2024-03-15 14:45:32 +0000

    added removeEventHandler to prevent jump on game over

--- src/components/bird.ts
+++ src/components/bird.ts
@@ -5,12 +5,21 @@ export class Bird {
   c: CanvasRenderingContext2D;
   width = 100;
   r = 30;
+  grav = 0.02
+  jumpEvent = (event: KeyboardEvent) => {
+    if (event.code === "ArrowUp") {
+      this.dy = -2
+    }
+  }
 
   constructor(c: CanvasRenderingContext2D) {
     this.x = 100;
     this.y = innerHeight / 2 - 200;
     this.c = c;
     this.dy = 0;
+    this.canJump = true
+
+    this.c.canvas.addEventListener("keydown", this.jumpEvent)
   }
 
   draw = () => {
@@ -21,8 +30,25 @@ export class Bird {
   }
 
   update = () => {
+    this.dy += this.grav
+    this.y += this.dy
+    if (this.y > innerHeight - this.r) {
+      this.grav = 0
+      this.dy = 0
+      console.log("fell off")
+    }
     this.draw()
   }
 
-  restart = () => {}
+  restart = () => {
+    this.grav = 0.02
+    this.y = innerHeight / 2
+    this.draw()
+  }
+
+  stop = () => {
+    this.dy = 0
+    this.grav = 0
+    this.c.canvas.removeEventListener("keydown", this.jumpEvent)
+  }
 }
